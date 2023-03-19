05fce947fb28ed800177f6b9862d4fcc1fd7f626
amator82 <ilyakorckishko@gmail.com>
2023-03-19 14:10:18 +0200

    add styles to section Explore and ExploreCard

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -5,7 +5,10 @@ import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
-  <motion.p variants={textContainer} className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
     {Array.from(title).map((letter, index) => (
       <motion.span key={index} variants={textVariant2}>
         {letter === ' ' ? '\u00A0' : letter}
@@ -14,6 +17,13 @@ export const TypingText = ({ title, textStyles }) => (
   </motion.p>
 );
 
-// export const TitleText = ({ title, textStyles }) => (
-//   <h2>Title Text</h2>
-// );
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} mt-[8px] font-bold md:text-[64px] text-[40px] text-white`}
+  >
+    {title}
+  </motion.h2>
+);
